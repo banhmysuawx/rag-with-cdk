31692314e44dc8d9d3288b055a824b762c18da38
Dinh Truong Quang <dinhtq@nal.vn>
2024-11-27 17:43:26 +0900

    first commit

--- bin/simple-rag-cdk.ts
+++ bin/simple-rag-cdk.ts
@@ -1,20 +1,18 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { SimpleRagCdkStack } from '../lib/simple-rag-cdk-stack';
+import { OpensearchBedrockRagCdkStack } from '../lib/opensearch-bedrock-rag-cdk-stack';
+import { EcsFargateCdkStack } from '../lib/ecs-fargate-cdk-stack';
 
+// Create a new app
 const app = new cdk.App();
-new SimpleRagCdkStack(app, 'SimpleRagCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// Create the stack
+const openSearchStack = new OpensearchBedrockRagCdkStack(app, 'OpensearchBedrockRagCdkStack', {
+});
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+// Create the ECS Fargate stack
+new EcsFargateCdkStack(app, 'EcsFargateCdkStack', {
+  OpenSearchEndpoint: openSearchStack.OpenSearchEndpoint,
+  VectorFieldName: openSearchStack.VectorFieldName,
+  VectorIndexName: openSearchStack.VectorIndexName
 });
